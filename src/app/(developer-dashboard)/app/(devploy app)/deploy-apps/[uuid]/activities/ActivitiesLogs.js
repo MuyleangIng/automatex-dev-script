30f37a9806a4a6270a1cd46c056a74bf82f24ffa
MuyleangIng <muyleanging@gmail.com>
2023-12-06 18:31:55 +0700

    fetch logs data from jenkins build logs

--- src/app/(developer-dashboard)/app/(devploy app)/deploy-apps/[uuid]/activities/ActivitiesLogs.js
+++ src/app/(developer-dashboard)/app/(devploy app)/deploy-apps/[uuid]/activities/ActivitiesLogs.js
@@ -1,20 +1,53 @@
-'use client'
-import React from 'react';
-import {useSession} from "next-auth/react";
-import {useSelector} from "react-redux";
-import {selectDeploymentApp, selectError, selectIsLoading} from "@/store/features/deploy-app/deployAppSlice";
-import {useGetConsoleLogsQuery} from "@/store/features/deploy-app/deployAppApiSlice";
+'use client';
+import React, { useState, useEffect, useRef } from 'react';
+import { useSession } from 'next-auth/react';
+import { useSelector } from 'react-redux';
+import {
+    selectDeploymentApp,
+    selectError,
+    selectIsLoading,
+} from '@/store/features/deploy-app/deployAppSlice';
+import { useGetConsoleLogsQuery } from '@/store/features/deploy-app/deployAppApiSlice';
+import { TfiReload } from 'react-icons/tfi';
+import { Button } from 'flowbite-react';
 
+function ActivitiesLogs({ params }) {
+    const { uuid } = params;
+    const [pollingInterval, setPollingInterval] = useState(null);
+    const [isPolling, setIsPolling] = useState(false);
+    const active = 'dark:!bg-gray-700 dark:!text-white';
+    const datav1 = useGetConsoleLogsQuery(uuid, { ...pollingInterval });
+    const containerRef = useRef();
 
-
-function ActivitiesLogs({params}) {
-    const {uuid} = params
-    const datav1 = useGetConsoleLogsQuery(uuid)
+    // useEffect to handle auto-scrolling when new data is fetched
+    useEffect(() => {
+        if (containerRef.current) {
+            containerRef.current.scrollTop = containerRef.current.scrollHeight;
+        }
+    }, [datav1?.error?.data]);
 
     return (
-        <pre>
-            {datav1.error.data}
-        </pre>
+        <div className={'mt-10 w-full rounded-xl border-dashed border-2 bg-white p-4 shadow dark:bg-gray-800 sm:p-6 xl:p-8'}>
+            <Button
+                color="gray"
+                size={'sm'}
+                onClick={() => {
+                    setIsPolling(!isPolling);
+                    setPollingInterval(isPolling ? null : { pollingInterval: 8000 });
+                }}
+                className={isPolling ? active : ''}
+            >
+                <TfiReload className={`mr-3 h-4 w-4 ${isPolling ? 'animate-spin' : ''}`} />
+                <p className={'text-xs'}>Polling</p>
+            </Button>
+            <div
+                ref={containerRef}
+                className="mx-auto p-2 rounded bg-gray-100 dark:bg-gray-900 bg-opacity-40 dark:bg-opacity-40 h-[100vh] overflow-auto space-y-5 scrollbar-thin scrollbar-thumb-gray-900 scrollbar-track-gray-100 scrollbar-rounded-full"
+            >
+                <pre>{datav1?.error?.data}</pre>
+            </div>
+        </div>
     );
 }
+
 export default ActivitiesLogs;
