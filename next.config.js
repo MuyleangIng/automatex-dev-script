4455559cfb32d38a940d8a3214bad10550a795a2
MuyleangIng <muyleanging@gmail.com>
2023-12-14 09:15:08 +0700

    update fix codec

--- next.config.js
+++ next.config.js
@@ -14,6 +14,9 @@ const nextConfig = {
             {
                 hostname: 'i.pinimg.com',
             },
+            {
+                hostname: 'wp-api.zipmex.com',
+            }
         ]
     }
 }
