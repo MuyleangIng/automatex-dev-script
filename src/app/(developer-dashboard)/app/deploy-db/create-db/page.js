67a99eaee93216d5e91f49b0be2c34573ef13b5e
p4nh4 <panhapos03@gmail.com>
2023-12-20 16:22:27 +0700

    db deploy page

---
+++ src/app/(developer-dashboard)/app/deploy-db/create-db/page.js
@@ -0,0 +1,14 @@
+'use client'
+import FormDB from '@/components/FormDB';
+import React from 'react';
+
+
+function Page() {
+    return (
+        <div>
+            <FormDB/>
+        </div>
+    );
+}
+
+export default Page;
