abe74b24b48606f805a4287fbe1d33fface3a7e3
samboleap3008 <samboleap3008@gmail.com>
2023-12-27 16:19:00 +0700

    fix: remove not use and remove component not completed❤️

--- src/store/features/gitlab/gitApi.js
+++ src/store/features/gitlab/gitApi.js
@@ -21,12 +21,9 @@ export const fetchProjectTree = createAsyncThunk(
     'projects/fetchProjectTree',
     async (id) => {
         const url = `${process.env.NEXT_PUBLIC_GITLAB_URL}/projects/${id}/repository/tree`;
-        console.log('Request URL:', url);
         try {
             const response = await baseQueryExport(url, { method: 'GET' });
-            console.log('Response list project:', response);
             const projectTree = await response.data;
-            console.log('Project Tree Data:', projectTree);
             return projectTree;
         } catch (error) {
             console.error('Error fetching GitLab project tree:', error.message);
